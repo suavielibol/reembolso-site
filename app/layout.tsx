977a3f61a1484c825f05a78f74b804c43d762c23
Ninja Scale <ninjascale@MacBook-Air-de-Ninja.local>
2025-07-01 10:02:52 -0300

    pixel alterado

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '656489137413556');
+          fbq('init', '1089564736393941');
           fbq('track', 'PageView');
         `}</Script>
 
@@ -54,7 +54,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=656489137413556&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1089564736393941&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
